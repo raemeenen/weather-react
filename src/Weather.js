0cdc74f1e932cfdaa5dbe0910d97adfe109a79f2
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-08-14 14:49:15 -0500

    added axios to use API

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather() {
+
+  function handleResponse(response) {
+    alert (`The weather in Kansas City is ${response.data.main.temp}°C`)
+  }
+
+  let apiKey = "3fd0b2514fdddeb5a1773faa623df844";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kansas City&units=imperial&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(handleResponse);
+}
